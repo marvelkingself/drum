f7b901895401f2956900f9eaf5fa9b0602cf7676
marvelkingself <barunkhan1234@gmail.com>
2023-08-17 21:44:26 +0530

    add file

---
+++ index.js
@@ -0,0 +1,61 @@
+var noOfButton = document.querySelectorAll(".drum").length;
+for (var i = 0; i < noOfButton; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function clickMe() {
+
+        var buttoninnerhtml = this.innerHTML;
+        drumSound(buttoninnerhtml);
+        animation(buttoninnerhtml);
+
+    });
+}
+document.addEventListener("keydown", function(event) {
+    drumSound(event.key);
+    animation(event.key);
+
+});
+function drumSound(buttonclick) {
+    switch (buttonclick) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+
+        case "l":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+
+        default:
+            console.log(buttoninnerhtml);
+            break;
+    }
+}
+function animation(key){
+    var activeButton=document.querySelector("."+key);
+    activeButton.classList.add("pressed");
+setTimeout(
+    function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
+
